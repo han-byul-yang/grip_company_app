c9dc78cc194c0083c507db4b59d9cf0ce261e2b8
Han Byul <yahb9@naver.com>
2022-05-11 03:30:22 +0900

    Fix: react-router-dom 6버전으로 다시 짜기

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 import BookMark from './routes/BookMark'
 import Search from './routes/Search'
 
 const Router = () => {
     return (
-      <HashRouter>
-        <Switch>
-          <Route path='/' exact component={Search} />
-          <Route path='/bookmark' exact component={BookMark} />
-          <Redirect from='*' to='/' />
-        </Switch>
-      </HashRouter>)
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Search />} />
+          <Route path='bookmark' element={<BookMark />} />
+        </Routes>
+      </BrowserRouter>
+    )
 }
 
 export default Router
+
+// router 구조 확실히 알고 고치기
