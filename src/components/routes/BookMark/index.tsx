306f5577689518de0fa0132b263618710197ce7e
Han Byul <yahb9@naver.com>
2022-05-12 22:13:05 +0900

    Fix: BookMark컴포넌트에서 Tab 컴포넌트랑 BookMarkModal 컴포넌트 위치 수정

--- src/components/routes/BookMark/index.tsx
+++ src/components/routes/BookMark/index.tsx
@@ -25,7 +25,7 @@ const BookMark = () => {
 
   const onDragEnd = ({destination, source}: DropResult) => {
     if (!destination)  return
-    
+
     setBookmarkedMovies((prevState) => {
       const copyMovie = [...prevState]
       const itemMove = copyMovie[source.index]
@@ -37,41 +37,38 @@ const BookMark = () => {
 
   return (
     <div className={styles.defaultStyle}>
+      <header>
+        <h1>내 즐겨찾기</h1>
+      </header>
       <DragDropContext onDragEnd={onDragEnd}>
         <div>
-          <header>
-            <h1>내 즐겨찾기</h1>
-          </header>
           <section>
-            { 
-        noBookmark ? 
-          <p>즐겨찾기가 존재하지 않습니다.</p> :
-          <Droppable droppableId="bookmarkDrop">
-            {(handleDrop) => (
-              <ul className={styles.resultList} ref={handleDrop.innerRef} {...handleDrop.droppableProps}>
-                {bookmarkedMovies.map((movie,idx) =>
-                  <Draggable draggableId={`${movie.imdbID}`} key={`${movie.imdbID}`} index={idx}> 
-                    {(handleDrag) => 
+            { noBookmark ? 
+              <p>즐겨찾기가 존재하지 않습니다.</p> :
+              <Droppable droppableId="bookmarkDrop">
+                {(handleDrop) => (
+                  <ul className={styles.resultList} ref={handleDrop.innerRef} {...handleDrop.droppableProps}>
+                    {bookmarkedMovies.map((movie,idx) =>
+                      <Draggable draggableId={`${movie.imdbID}`} key={`${movie.imdbID}`} index={idx}> 
+                        {(handleDrag) => 
                         // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-                      <li key={`movie-${movie.imdbID}`} className={styles.eachResult} ref={handleDrag.innerRef} {...handleDrag.draggableProps} {...handleDrag.dragHandleProps} onClick={() => handleMovieClick(movie)}>
-                        <img src={movie.Poster} alt='movie poster'/>
-                        <div className={styles.contents}>
-                          <div className={styles.title}>{movie.Title}</div>
-                          <span className={styles.type}>{movie.Type}</span> |
-                          <span className={styles.year}>{movie.Year}</span>
-                        </div>
-                      </li>}
-                  </Draggable>)}
-                {handleDrop.placeholder}
-              </ul>)}
-          </Droppable>
-
-        }
+                          <li key={`movie-${movie.imdbID}`} className={styles.eachResult} ref={handleDrag.innerRef} {...handleDrag.draggableProps} {...handleDrag.dragHandleProps} onClick={() => handleMovieClick(movie)}>
+                            <img src={movie.Poster} alt='movie poster'/>
+                            <div className={styles.contents}>
+                              <div className={styles.title}>{movie.Title}</div>
+                              <span className={styles.type}>{movie.Type}</span> |
+                              <span className={styles.year}>{movie.Year}</span>
+                            </div>
+                          </li>}
+                      </Draggable>)}
+                    {handleDrop.placeholder}
+                  </ul>)}
+              </Droppable>}
           </section>
-          <Tabs />
-          <BookMarkModal openModal={openModal} setOpenModal={setOpenModal} state='forDelete' />
         </div>
       </DragDropContext>
+      <Tabs />
+      <BookMarkModal openModal={openModal} setOpenModal={setOpenModal} state='forDelete' />
     </div>
   )
 }
